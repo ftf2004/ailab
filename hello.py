0dbb2fe4e634f1b1271be496d5988a1014f29658
ftf2004 <frickthefasak@outlook.com>
2024-07-23 09:07:38 +0000

    done

--- hello.py
+++ hello.py
@@ -1,4 +1,28 @@
 def vacuum_world():
-    goal_state={'A':'0','B':'0'}
-    cost=0
-    locat
+    goal_state = {'A': '0', 'B': '0'}  # 0 indicates Clean, 1 indicates Dirty
+    cost = 0
+
+    loc = input("Enter Location of Vacuum: ")
+    status = input("Enter status: ")
+    status_compliment = input("Enter status of other room: ")
+
+    if status == '1':
+        print(f"Location {loc} is Dirty.")
+        goal_state[loc] = '0'
+        cost += 1
+        print(f"Cost for CLEANING {loc} " + str(cost))
+
+    if status_compliment == '1':
+        otherloc = 'B' if loc == 'A' else 'A'
+        print(f"Location {otherloc} is Dirty.")
+        cost += 1
+        print("Moving to other Location. Cost for moving " + str(cost))
+        goal_state[otherloc] = '0'
+        cost += 1
+        print(f"Cost for CLEANING {otherloc}: " + str(cost))
+
+    print("GOAL STATE: ")
+    print(goal_state)
+    print("Performance Measurement: " + str(cost))
+
+vacuum_world()
