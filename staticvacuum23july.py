c98919343846423dc4e023516dce32faa0630e41
ftf2004 <frickthefasak@outlook.com>
2024-07-30 09:15:27 +0000

    done

---
+++ staticvacuum23july.py
@@ -0,0 +1,13 @@
+if location=='A':
+    isClean(location,status)
+    update(location_status)
+else:
+    isClean(location,status)
+    update(location_status)
+
+function isClean(location,status):
+    if status==1;
+
+function update(location_status):
+    if location_status==1:
+        location_status=0
